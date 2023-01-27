db73ea97f585373efab52d4261ff71f33f8e2e0a
Aria Hemin <ariahemin@gmail.com>
2023-01-27 12:46:08 +0300

    intial project set up

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+
+function App() {
+ 
+  return (
+    <div className="text-4xl flex items-center justify-center">
+      panic mode !
+    </div>
+  )
+}
+
+export default App
